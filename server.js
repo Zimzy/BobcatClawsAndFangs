eddfcbbab513d945a1be307cd3e7f658313e801b
yperez20 <145092708+yperez20@users.noreply.github.com>
2025-10-26 20:22:24 -0500

    Revert "Merge main and landing branches"

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/*', (req, res) => {
 });
 
 // Default port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
