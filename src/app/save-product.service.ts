2ffd4a5ad1709efcb67786b20a9ce92b79232e38
yperez20 <145092708+yperez20@users.noreply.github.com>
2025-12-04 02:23:49 -0600

    Update save-product.service.ts
    Changes were made in order to have refresh error no longer occur

--- src/app/save-product.service.ts
+++ src/app/save-product.service.ts
@@ -1,50 +1,145 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class SaveProductService {
+//    productValue: any = null;
+//    productCategory: any = null;
+//    keyword: any = null;
+//    cart: any[] = [];
+   
+//    // saves clicked on product for detailed product page functionality 
+//    saveProductService(value: any){
+//     this.productValue = value;
+//    }
+
+
+//    saveCart(){
+//     this.cart.push(this.productValue);
+//    }
+
+//    getCart(){
+//     return this.cart;
+//    }
+
+//    //saves keyword to display on categories page
+//    saveKeyword(value: any){
+//     this.keyword = value;
+//    }
+//    getKeyword(){
+//     return this.keyword;
+//    }
+//    //saves JSON of category for categories page
+//    saveCatService(value: any){
+//     this.productCategory = value;
+//     //console.log('cat',this.productCategory)
+//    }
+//    getCat(){
+//     console.log("Cat:",this.productCategory);
+//     return this.productCategory;
+//    }
+//    getProduct(){
+//     //console.log('value', this.productValue)
+//     return this.productValue;
+//    }
+
+   
+//   constructor() { }
+// }
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SaveProductService {
-   productValue: any ;
-   productCategory: any;
-   keyword: any;
-   cart: any[] = [];
-   
-   // saves clicked on product for detailed product page functionality 
-   saveProductService(value: any){
+  productValue: any = null;
+  productCategory: any = null;
+  keyword: any = null;
+  cart: any[] = [];
+
+  constructor() {
+    // 🔹 Restore cart
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+      try {
+        this.cart = JSON.parse(storedCart);
+      } catch {
+        this.cart = [];
+      }
+    }
+
+    // 🔹 Restore last selected product
+    const storedProduct = localStorage.getItem('currentProduct');
+    if (storedProduct) {
+      try {
+        this.productValue = JSON.parse(storedProduct);
+      } catch {
+        this.productValue = null;
+      }
+    }
+
+    // 🔹 Restore category results
+    const storedCategory = localStorage.getItem('categoryProducts');
+    if (storedCategory) {
+      try {
+        this.productCategory = JSON.parse(storedCategory);
+      } catch {
+        this.productCategory = null;
+      }
+    }
+
+    // 🔹 Restore keyword
+    const storedKeyword = localStorage.getItem('keyword');
+    if (storedKeyword) {
+      this.keyword = storedKeyword;
+    }
+  }
+
+  // saves clicked on product for detailed product page functionality 
+  saveProductService(value: any) {
     this.productValue = value;
-   }
+    localStorage.setItem('currentProduct', JSON.stringify(value));
+  }
 
-
-   saveCart(){
+  saveCart() {
+    if (!this.productValue) {
+      return;
+    }
     this.cart.push(this.productValue);
-   }
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
 
-   getCart(){
+  getCart() {
     return this.cart;
-   }
+  }
 
-   //saves keyword to display on categories page
-   saveKeyword(value: any){
+  // saves keyword to display on categories page
+  saveKeyword(value: any) {
     this.keyword = value;
-   }
-   getKeyword(){
+    localStorage.setItem('keyword', value);
+  }
+
+  getKeyword() {
     return this.keyword;
-   }
-   //saves JSON of category for categories page
-   saveCatService(value: any){
+  }
+
+  // saves JSON of category for categories page
+  saveCatService(value: any) {
     this.productCategory = value;
+    localStorage.setItem('categoryProducts', JSON.stringify(value));
     //console.log('cat',this.productCategory)
-   }
-   getCat(){
-    console.log("Cat:",this.productCategory);
+  }
+
+  getCat() {
+    console.log('Cat:', this.productCategory);
     return this.productCategory;
-   }
-   getProduct(){
+  }
+
+  getProduct() {
     //console.log('value', this.productValue)
     return this.productValue;
-   }
-
-   
-  constructor() { }
+  }
 }
+
